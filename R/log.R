b9c5b303dbd025f243ec1c6d6c0b50e202f00ae3
Odran Sourdeval <odran.sourdeval@uni-leipzig.de>
2020-02-24 16:18:17 +0100

    added parameter description

--- R/log.R
+++ R/log.R
@@ -1,11 +1,24 @@
-
+#' Standardized log file output
+#'
+#' @param logtext string, text to be exported in the log file
+#' @param logfn string, name of the log file
+#' @param lev integer, indenting space to be added before `logtext'.
+#' Default is 0.
+#' @param dots logical, indicates if an ellipsis should be added at the end of `logtext'.
+#' Default is FALSE.
+#' @param newline logical, indicates if a new line should be added after `logtext'.
+#' Default is FALSE.
+#' @param append logical, indicates if the log file should be appended, otherwise an existing file would be replaced.
+#' Default is TRUE.
+#' @return
+#' NULL
 #' @export
-logfile <- function(...,fn,lev=0,append=TRUE,dots=FALSE,newline=FALSE) {
+logfile <- function(logtext,logfn,lev=0,dots=FALSE,newline=FALSE,append=TRUE) {
     pre <- rep(" ",lev); post <- ""
     if(dots) post <- paste0(post,"...")
     if(newline) post <- paste0(post,"\n")
 
-    cat(pre,...,post,file=fn,append=append)
+    cat(pre,logtext,post,file=logfn,append=append)
 
     return(NULL)
 }
